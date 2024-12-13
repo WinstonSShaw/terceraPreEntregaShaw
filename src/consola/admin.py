48ac7a926850856354a8335a8d15951556ce06c4
WinstonSShaw <winstonsshaw@hotmail.com>
2024-12-12 22:53:39 -0300

    modelos iniciales

--- src/consola/admin.py
+++ src/consola/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from .models import Consola, Juego, Jugador
+
 # Register your models here.
+
+@admin.register(Consola)
+class ConsolaAdmin(admin.ModelAdmin):
+    list_display = ("nombre",)
+
+@admin.register(Juego)
+class JuegoAdmin(admin.ModelAdmin):
+    list_display = ("consola__nombre", "nombre_juego", "fecha_inicio",)
+
+@admin.register(Jugador)
+class JugadorAdmin(admin.ModelAdmin):
+    list_display = ("juego", "dni",)
+
--- src/consola/models.py
+++ src/consola/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Consola(models.Model):
+    nombre = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.nombre
+
+class Juego(models.Model):
+    consola = models.ForeignKey(Consola, on_delete=models.SET_NULL, null=True)
+    nombre_juego = models.CharField(max_length=255)
+    fecha_inicio = models.DateField(null=True)
+
+    def __str__(self):
+        return f"{self.consola} - {self.nombre_juego}"
+
+class Jugador(models.Model):
+    juego = models.ForeignKey(Juego, on_delete=models.SET_NULL, null=True)
+    dni = models.IntegerField()
