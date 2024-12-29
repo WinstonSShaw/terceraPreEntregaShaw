395cbe229c581a69d557d7e2bdd9fe31648dc60e
WinstonSShaw <winstonsshaw@hotmail.com>
2024-12-28 22:16:39 -0300

    completo crud con editar y eliminar

--- src/consola/urls.py
+++ src/consola/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path
 
-from .views import about, index, consola_list, juego_list, jugador_list, consola_create, juego_create, jugador_create
+from .views import about, index, consola_list, juego_list, jugador_list, consola_create, juego_create, jugador_create, consola_edit, consola_delete, juego_edit, juego_delete, jugador_edit, jugador_delete
 
 app_name = "consola"
 
@@ -14,5 +14,11 @@ urlpatterns = [
     path("juego/create/", juego_create, name="juego_create"),
     path("jugador/list/", jugador_list, name="jugador_list"),
     path("jugador/create/", jugador_create, name="jugador_create"),
+    path("consola/<int:pk>/edit/", consola_edit, name="consola_edit"),
+    path("consola/<int:pk>/delete/", consola_delete, name="consola_delete"),
+    path("juego/<int:pk>/edit/", juego_edit, name="juego_edit"),
+    path("juego/<int:pk>/delete/", juego_delete, name="juego_delete"),
+    path("jugador/<int:pk>/edit/", jugador_edit, name="jugador_edit"),
+    path("jugador/<int:pk>/delete/", jugador_delete, name="jugador_delete"),
 ]
 
--- src/consola/views.py
+++ src/consola/views.py
@@ -25,6 +25,24 @@ def consola_create(request):
             return redirect("consola:consola_list")
     return render(request, "consola/consola_form.html", {"form": form})
 
+def consola_edit(request, pk):
+    consola = Consola.objects.get(pk=pk)
+    if request.method == "POST":
+        form = ConsolaForm(request.POST, instance=consola)
+        if form.is_valid():
+            form.save()
+            return redirect("consola:consola_list")
+    else:
+        form = ConsolaForm(instance=consola)
+    return render(request, "consola/consola_form.html", {"form": form, "consola": consola})
+
+def consola_delete(request, pk):
+    consola = Consola.objects.get(pk=pk)
+    if request.method == "POST":
+        consola.delete()
+        return redirect("consola:consola_list")
+    return render(request, "consola/consola_confirm_delete.html", {"consola": consola})
+
 def juego_list(request):
     query = Juego.objects.all()
     context = {"juegos": query}
@@ -40,6 +58,24 @@ def juego_create(request):
             return redirect("consola:juego_list")
     return render(request, "consola/juego_form.html", {"form": form})
 
+def juego_edit(request, pk):
+    juego = Juego.objects.get(pk=pk)
+    if request.method == "POST":
+        form = JuegoForm(request.POST, instance=juego)
+        if form.is_valid():
+            form.save()
+            return redirect("consola:juego_list")
+    else:
+        form = JuegoForm(instance=juego)
+    return render(request, "consola/juego_form.html", {"form": form, "juego": juego})
+
+def juego_delete(request, pk):
+    juego = Juego.objects.get(pk=pk)
+    if request.method == "POST":
+        juego.delete()
+        return redirect("consola:juego_list")
+    return render(request, "consola/juego_confirm_delete.html", {"juego": juego})
+
 def jugador_list(request):
     query = Jugador.objects.all()
     context = {"jugadores": query}
@@ -54,3 +90,21 @@ def jugador_create(request):
             form.save()
             return redirect("consola:jugador_list")
     return render(request, "consola/jugador_form.html", {"form": form})
+
+def jugador_edit(request, pk):
+    jugador = Jugador.objects.get(pk=pk)
+    if request.method == "POST":
+        form = JugadorForm(request.POST, instance=jugador)
+        if form.is_valid():
+            form.save()
+            return redirect("consola:jugador_list")
+    else:
+        form = JugadorForm(instance=jugador)
+    return render(request, "consola/jugador_form.html", {"form": form, "jugador": jugador})
+
+def jugador_delete(request, pk):
+    jugador = Jugador.objects.get(pk=pk)
+    if request.method == "POST":
+        jugador.delete()
+        return redirect("consola:jugador_list")
+    return render(request, "consola/jugador_confirm_delete.html", {"jugador": jugador})
